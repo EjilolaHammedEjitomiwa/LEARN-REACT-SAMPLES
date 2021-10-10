4eec21724596e88b3273a1c366995e02f5fe0057
EjilolaHammedEjitomiwa <geotechbrain@gmail.com>
2021-10-10 09:49:51 +0100

    useContext

--- src/tutorial/6-useReducer/setup/Modal.js
+++ src/tutorial/6-useReducer/setup/Modal.js
@@ -1,18 +1,14 @@
 import React, { useEffect } from "react";
 
-const Modal = (props) => {
-
+const Modal = ({ modalContent, closeModal }) => {
   useEffect(() => {
     setTimeout(() => {
-      props.closeModal()
-    },3000)
-   
-  })
-
-
+      closeModal();
+    }, 3000);
+  });
   return (
     <div className="modal">
-      <p>{props.modalContent}</p>{" "}
+      <p>{modalContent}</p>
     </div>
   );
 };
--- src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -1,10 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { data } from "../../../data";
 
 // more components
 // fix - context api, redux (for more complex cases)
 
 const PropDrilling = () => {
-  return <h2>prop drilling</h2>;
+  const [people, setPeople] = useState(data);
+
+  const removePerson = (id) => {
+    return people.filter((person) => person.id === id);
+  };
+
+  return (
+    <section>
+      <h3>Prop Drilling</h3>
+      <List people={people} removePerson={removePerson} />
+    </section>
+  );
+};
+
+const List = (props) => {
+  return (
+    <>
+      {props.people.map((person) => {
+        return (
+          <SinglePerson
+            key={person.id}
+            {...person}
+            removePerson={props.removePerson}
+          />
+        );
+      })}
+    </>
+  );
+};
+
+const SinglePerson = (props) => {
+  return (
+    <div className="item">
+      <h4> {props.name} </h4>
+      <button className="btn" onClick={() => props.removePerson(props.id)}>
+        Remove
+      </button>
+    </div>
+  );
 };
 
 export default PropDrilling;
