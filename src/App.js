b81dde144c40055e6630cdf4b3c0bec27532702f
EjilolaHammedEjitomiwa <geotechbrain@gmail.com>
2021-10-10 10:49:52 +0100

    PropTypes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./tutorial/9-custom-hooks/setup/1-fetch-example";
+import Setup from "./tutorial/10-prop-types/setup";
 
 function App() {
   return (
@@ -11,4 +11,4 @@ function App() {
 
 export default App;
 
-// 7:20:48
+// 9:00
--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
+import defaultImage from "../../../assets/default-image.jpeg";
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+const Product = ({ name, price, image }) => {
+  const url = image && image.url;
+  return (
+    <article className="product">
+      <img src={url || defaultImage} alt="" />
+      <h4> {name} </h4>
+      <h4> $ {price || 3.99} </h4>
+    </article>
+  );
 };
+
+// Product.propTypes = {
+//   image: PropTypes.object.isRequired,
+//   name: PropTypes.string.isRequired,
+//   price: PropTypes.number.isRequired,
+// };
 
 export default Product;
