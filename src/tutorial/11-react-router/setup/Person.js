95e9c2818bc069698548d304fa03cf025b156588
EjilolaHammedEjitomiwa <geotechbrain@gmail.com>
2021-10-10 17:59:32 +0100

    React Router

--- src/tutorial/11-react-router/setup/Person.js
+++ src/tutorial/11-react-router/setup/Person.js
@@ -1,7 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { data } from '../../../data';
-import { Link, useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { data } from "../../../data";
+import { Link, useParams } from "react-router-dom";
 const Person = () => {
+  const { id } = useParams();
+
+  const [name, setName] = useState("default name");
+
+  console.log(`ID =  ${id}`);
   return (
     <div>
       <h2>person</h2>
